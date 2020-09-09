66147ffe327ae4d5e2791c57642ef8cacf75a2ff
craighillelson <craighillelson@gmail.com>
2020-09-09 00:02:59 -0400

    update last_next_monday.py
    simplified code

--- last_next_monday.py
+++ last_next_monday.py
@@ -1,39 +1,28 @@
 """Get the dates for the previous and next Mondays."""
 
 from datetime import (date,
-                      datetime,
-                      timedelta)
-from dateutil.relativedelta import relativedelta
-from dateutil.rrule import MO
+                      datetime)
+from dateutil.relativedelta import (relativedelta,
+                                    MO)
+
+
+def week_from_today(int_1, int_2):
+    """Find next Monday's date."""
+    datetime_obj = TODAY + relativedelta(days=+int_1, weekday=MO(+int_2))
+    return datetime_obj.date()
+
+
+def week_ago_today():
+    """Find last Monday's date."""
+    datetime_obj = TODAY + relativedelta(weekday=MO(-1))
+    return  datetime_obj.date()
+
 
 TODAY = datetime.now()
 
-
-def adjacent_mondays(num_weeks):
-    """
-    Given an intenger representing the number of weeks in either direction
-    (greater than zero representing Mondays in the future, less than zero
-    representing Mondays in the past), calculate the date of that Monday.
-    """
-    return TODAY + relativedelta(weekday=MO(num_weeks))
-
-
-def output_date(label, mon_date):
-    """Print a specified Monday's date."""
-    print(f"{label}: {mon_date.strftime('%Y-%m-%d')}")
-
-
-output_date("\nToday", TODAY)
-
 if date.today().weekday() == 0:
-    next_monday_delta = 2
-    last_monday_delta = -2
+    print(week_from_today(1, 1))
+    week_ago_today()
 else:
-    next_monday_delta = 1
-    last_monday_delta = -1
-
-next_monday = adjacent_mondays(next_monday_delta)
-output_date("Next Monday", next_monday)
-last_monday = adjacent_mondays(last_monday_delta)
-output_date("Last Monday", last_monday)
-print("\n")
+    print(week_from_today(0, 1))
+    print(week_ago_today())
