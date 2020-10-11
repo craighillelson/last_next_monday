844aeabbc989bb4aeb16db5fc60268de1beecfce
Craig Hillelson <craighillelson@gmail.com>
2020-10-11 12:22:25 -0400

    update last_next_monday.py
    linted code

--- last_next_monday.py
+++ last_next_monday.py
@@ -21,8 +21,8 @@ def week_ago_today():
 TODAY = datetime.now()
 
 print(f"\nlast Monday's date: {week_ago_today()}")
-next_mon_label = "next Monday's date:"
+NEXT_MON_LABEL = "next Monday's date:"
 if date.today().weekday() == 0:
-    print(f"{next_mon_label} {week_from_today(1, 1)}\n")
+    print(f"{NEXT_MON_LABEL} {week_from_today(1, 1)}\n")
 else:
-    print(f"{next_mon_label} {week_from_today(0, 1)}\n")
+    print(f"{NEXT_MON_LABEL} {week_from_today(0, 1)}\n")
